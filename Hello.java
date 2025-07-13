d9e685473fc09b36ead87fdbc367351576f41550
correcode <aliba.truta@gmail.com>
2025-07-13 12:58:09 -0300

    numeros primos
    Signed-off-by: correcode <aliba.truta@gmail.com>

--- Hello.java
+++ Hello.java
@@ -1,18 +1,23 @@
 public class Hello{
     public static void main(String[] args) {
 
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10};
 
-        int[] numeros = {1,2,3,4,5,6,7,8};
-        boolean temPar = false;
-
+        System.out.println("Numero primo:  ");
         for(int numero : numeros){
-            if (numero % 2 == 0){
-                System.out.println("Numero par: " + numero + " : ");
-                temPar = true;
+            if(ehPrimo(numero)){
+                System.out.println(numero + " ");
             }
-        } if (!temPar){
-            System.out.println("Não há numeros pares no array");
-           }
+        }
+
+    }
+    public static boolean ehPrimo(int n){
+        if(n <= 1) return false;
+
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0) return false;
+        }
+        return true;
     }
 }
 
