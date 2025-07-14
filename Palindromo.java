4f9342fb6a45b6a98df0cd002e64288ea52f4ce2
correcode <aliba.truta@gmail.com>
2025-07-14 12:19:29 -0300

    refeito palindromo
    Signed-off-by: correcode <aliba.truta@gmail.com>

--- Palindromo.java
+++ Palindromo.java
@@ -2,19 +2,18 @@ public class Palindromo {
 
     public static void main(String[] args) {
 
-        String palavra1 = "casa";
-        String palavra2 = "arara";
+        String palavra1 = "ovo";
+        String palavra2 = "cobra";
 
-        System.out.println(palavra1 + " é palindromo? " + ehPalindromo(palavra1));
-
-        System.out.println(palavra2 + " é palindromo? " + ehPalindromo(palavra2));
+        System.out.println(palavra1 + " é palindromo ? " + ehPalindromo(palavra1));
+        System.out.println(palavra2 + " é palindromo ? " + ehPalindromo(palavra2));
 
     }
 
     public static boolean ehPalindromo(String texto) {
 
         int esquerdo = 0;
-        int direito = texto.length() - 1;
+        int direito = texto.length() -1;
 
         while (esquerdo < direito) {
             if (texto.charAt(esquerdo) != texto.charAt(direito)) {
